5ae7d4a2a0cd588020f1f8e576308495d06f9da8
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-09 06:31:50 -0700

    last energy

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print lowercase alphabet excluding c and q
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 'a'; c <= 'z'; ++c)
+{
+if (c != 'a' && c != 'q')
+putchar(c);
+}
+
+putchar('\n');
+return (0);
+}
