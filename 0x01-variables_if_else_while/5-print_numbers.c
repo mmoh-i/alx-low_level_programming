c3779ca8f53b06cd98b2a349896ad19029a19991
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-09 06:39:53 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers from 0 to 9on one line
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+
+for (i = '0'; i <= '9'; ++i)
+putchar(i);
+
+putchar('\n');
+
+return (0);
+}
