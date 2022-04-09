71e5377793621a0306dd6cfaafe5d9a8c950bab4
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-09 02:24:24 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main -entry point
+ *
  * Returns: 0 after printing the function
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
+} 
+
