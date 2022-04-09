2fa0307cbb6a5255d032979a8942da02fa7fec14
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-09 03:18:29 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ char d;
 float f;
 
 printf("size of a char: %1u byte(s)\n", (unsigned long)sizeof(d));
-printf("size of an  int: %1u byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %1u byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(c));
 printf("size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
