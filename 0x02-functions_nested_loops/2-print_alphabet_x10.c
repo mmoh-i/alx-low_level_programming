9e5ddd8259d22c0efbcca0d03665e8124c2d026d
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-12 23:07:34 -0700

    Tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,27 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - using the main.h header
+ * print_alphabet_x10 - print alphabet
  *
- * Return: functionn with alphabet 10X
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
-int count = 0;
 int alphabet;
+int count;
 
+
+count = 0;
 while (count < 10)
 {
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-_putcahr(alphabet);
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
 }
 
 count++;
-
 _putchar('\n');
+}
+
 
 }
