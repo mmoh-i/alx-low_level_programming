3233e2ae05d263eedf2fc3a64fb732f7c7068bba
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-12 20:26:30 -0700

    Task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * mian - Print alphabet
+ * print_alphabet - print alphabet lowercase
  *
- * Retru: Always 0.
+ * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
 char alphabet;
+
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
 
 _putchar('\n');
+
 }
