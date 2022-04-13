5b822dc9a2b48bfff1df0e5c07f1df9c014611a0
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-13 00:48:12 -0700

    Taask 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - function that computes ths absolute value of an intger
+ *
+ * Return :Always 0
+ */
+int _abs(int n) 
+{
+if (n < 0)
+{
+return (n * (-1));
+}
+
+else if (n == 0)
+{
+return (0);
+}
+
+else
+{
+return (n);
+
+}
+
+}
