66b3c02077d20069b9f795097608dcc15792d82e
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-13 19:27:02 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+int n;
+while (n <= 98)
+{
+if (n = 0; n > 98; n++)
+{
+_Putchar (n);
+_putchar (',');
+}
+
+}
+return (0);
+}
