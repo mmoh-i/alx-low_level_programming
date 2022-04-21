ad05cb326ff55d72397cce91767e65cf59142ae9
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-20 21:08:23 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_prev - prints a string and a new line
+ *
+ * Return : a new line
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	length;
+	
+	for (i = 0, s[length] != '\0'; legth++)
+	{
+	}
+
+	for (i = length -1; i >= 0; i--)
+	{
+		_putchar(s[i])
+	}
+	_putchar('\n');
+}
