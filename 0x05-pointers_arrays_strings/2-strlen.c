c2067f2e12362c836bedc5036265e7ef86222359
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-20 20:35:05 -0700

    The task explains

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
 /**
- * _strlen - calculate the length os a string.
+ * _strlen - calculate the length of a string.
  *
- * @s: the string to calculate its length.
- * Return: lenght of a strig
+ * @s: the string to calculate it's length.
+ * Return: length of a string.
  */
+
 int _strlen(char *s)
 {
 	int i;
 	int length = 0;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		length++;
 	}
 	return (length);
 }
+© 2022 GitHub, Inc.
+Terms
+Privacy
+
