d4a75b5b3249e2eff554bfbf1fff54e250833e31
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-20 21:12:06 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,13 @@ void print_rev(char *s)
 	int i = 0;
 	length;
 	
-	for (length = 0, s[length] != '\0'; legth++)
+	for (length = 0, s[length] != '\0'; length++)
 	{
 	}
 
 	for (i = length -1; i >= 0; i--)
 	{
-		_putchar(s[i])
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
