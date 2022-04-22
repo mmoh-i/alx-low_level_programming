8c57b3bb6f8daaceed420e8a287a800b338a42dd
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-21 18:23:50 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,39 +1,63 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
-
-/**
- * main - print password.
- *
- * Return: 0.
- */
-
-int main(void)
+  
+// Function to randomly generates password
+// of length N
+void randomPasswordGeneration(int N)
 {
-	int ascii = 2772, i = 0, j, random;
-	char password[100];
-	time_t t;
-
-	srand((int) time(&t));
-	while (ascii > 126)
-	{
-		random = rand() % 126;
-		password[i] = random;
-		ascii -= random;
-		i++;
-	}
-	if (ascii > 0)
-		password[i] = ascii;
-	else
-	{
-		i--;
-	}
-
-
-	for (j = 0; j <= i; j++)
-	{
-		printf("%c", password[j]);
-	}
-	return (0);
+    // Initialize counter
+    int i = 0;
+  
+    int randomizer = 0;
+  
+    // Seed the random-number generator
+    // with current time so that the
+    // numbers will be different every time
+    srand((unsigned int)(time(NULL)));
+  
+    // Array of numbers
+    char numbers[] = "0123456789";
+  
+    // Array of small alphabets
+    char letter[] = "abcdefghijklmnoqprstuvwyzx";
+  
+    // Array of capital alphabets
+    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+  
+    // Array of all the special symbols
+    char symbols[] = "!@#$^&*?";
+  
+    // Stores the random password
+    char password[N];
+  
+    // To select the randomizer
+    // inside the loop
+    randomizer = rand() % 4;
+  
+    // Iterate over the range [0, N]
+    for (i = 0; i < N; i++) {
+  
+        if (randomizer == 1) {
+            password[i] = numbers[rand() % 10];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 2) {
+            password[i] = symbols[rand() % 8];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else if (randomizer == 3) {
+            password[i] = LETTER[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+        else {
+            password[i] = letter[rand() % 26];
+            randomizer = rand() % 4;
+            printf("%c", password[i]);
+        }
+    }
 }
