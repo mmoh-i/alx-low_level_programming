9cd893f062726aefe62086120c908277080f824d
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-04-22 02:51:13 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,63 +1,32 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-  
-// Function to randomly generates password
-// of length N
-void randomPasswordGeneration(int N)
+
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: always 0
+ */
+int main(void)
 {
-    // Initialize counter
-    int i = 0;
-  
-    int randomizer = 0;
-  
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
-  
-    // Array of numbers
-    char numbers[] = "0123456789";
-  
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-  
-    // Array of all the special symbols
-    char symbols[] = "!@#$^&*?";
-  
-    // Stores the random password
-    char password[N];
-  
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-  
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-  
-        if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
+char a[100];
+	int r, n, i;
+
+	n = 0;
+	i = 0;
+	srand(time(NULL));
+	while (n < 2645)
+	{
+		r = rand() % 122;
+		if (r > 32)
+		{
+			a[i++] = r;
+			n += r;
+		}
+	}
+	a[i++] = (2772 - n);
+	a[i] = '\0';
+	printf("%s", a);
+
+	return (0);
 }
