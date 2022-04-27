51b60a51f0200dbcf00ab0bf9599eea71ae884d3
mmoh-i <ibrahimmusty100@yahoo.com>
2022-04-27 06:13:45 -0700

    chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,12 +7,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j;
+	int i, j;
 
-	for (; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
+		{
 			_putchar(a[i][j]);
+		}
 		_putchar('\n');
 	}
 }
