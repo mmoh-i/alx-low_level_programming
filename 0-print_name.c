a26a9ed121b6bbf71694974bec7d83e9e69a45d6
mmoh-i <ibrahimmusty100@yahoo.com>
2022-05-13 20:12:44 -0700

    Task 0

---
+++ 0-print_name.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * print_name - print name as is
+ * @name: name of the person
+ *
+ * Return: Nothing
+ */
+void print_name(char *name)
+{
+	printf("Hello, my name is %s\n", name);
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 hello c 
 nested C loops
 Na the README be this
+Pointers and arrays
+malloc
+Function Pointers
