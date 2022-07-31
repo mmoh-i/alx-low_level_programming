b15effd5232edf2cd74d7f0d809045ea001a19a2
mmoh-i <ibrahimmusty100@yahoo.com>
2022-07-31 04:47:05 -0700

    hash

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,7 +1,6 @@
 #include "hash_tables.h"
 
-#define INIT 5381
-#define MULT 33
+
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
@@ -12,9 +11,9 @@ unsigned long int hash_djb2(const unsigned char *str){
 	u_int32_t hash;
 	int  c;
 
-	hash = INIT;
+	hash = 5381;
 	while((c = *str++)){
-		hash = hash * MULT + c;
+		hash = ((hash << 5) + hash + c);
 	}
-	return hash;
+	return (hash);
 }
