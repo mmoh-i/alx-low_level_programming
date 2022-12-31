2e9a3ba194872e318cf7a380f2e67220d6f1f4a9
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-12-31 09:54:46 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,20 @@
-/**
- * listint_len - returns the number of elements in a linked listint_t list.
- * @h: head of a node
- * Return:returns the number of elements in a linked list
- */
-
 #include "lists.h"
 
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
-size_t count = 0;
-const listint_t *node = h;
+	size_t num = 0;
 
-while (node != NULL)
-{
-node = node->next;
-count++;
-}
-return (count);
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
 }
